c1e6882afbf4c6ef36edabad73e9fa2e0d237196
Prajapati Kalu <kaluprajapati1907@gmail.com>
2026-06-29 00:14:04 +0530

    commit

--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://ai-chatbot-bmsy.onrender.com",
+      origin: "http://localhost:5173",
     withCredentials: true,
 })
 
